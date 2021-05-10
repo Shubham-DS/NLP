903490abcc03957567265b8b44dec6181991f269
Shubh9391 <svshuvam@gmail.com>
2021-05-11 03:39:21 +0530

    2nd version of Test

--- Test.py
+++ Test.py
@@ -1,2 +1,8 @@
-print('I like NLP')
+Interest=input('Enter the field you like: ')
+if Interest=='NLP':
+    print('I like NLP')
+elif Interest=='Power_BI':
+    print('I like Power_BI')
+else:
+    print('I love Data Science')
 
